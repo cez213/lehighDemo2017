e592661c3b4234ad1199d045ca32d95e17fc503f
carolynz <carolynz@google.com>
2017-09-25 12:42:11 -0400

    create app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MdButtonModule, MdInputModule, MdTableModule} from '@angular/material';
+import {HttpModule} from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { SuppliesComponent } from './supplies/supplies.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  exports: [
+    MdInputModule,
+    MdButtonModule,
+    MdTableModule,
+  ]
+})
+export class AppMaterialModule {}
+
+@NgModule({
+  declarations: [AppComponent, SuppliesComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpModule,
+    AppMaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, SuppliesComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/supplies/supplies.component.ts
+++ src/app/supplies/supplies.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+
+const EMAIL_REGEX =
+    /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
 @Component({
   selector: 'app-supplies',
@@ -6,10 +10,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./supplies.component.css']
 })
 export class SuppliesComponent implements OnInit {
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.pattern(EMAIL_REGEX)]);
+
+  beverageForm = new FormGroup({
+    name: new FormControl('', [
+      Validators.required,
+    ]),
+    quantity: new FormControl('', [
+      Validators.required,
+    ])
+  });
 
   constructor() { }
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
