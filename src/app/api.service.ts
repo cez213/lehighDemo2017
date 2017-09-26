9415dce922b1773b589bac955967b361baa04a51
carolynz <carolynz@google.com>
2017-09-26 15:20:04 -0400

    add local storage for api url

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,34 +1,34 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/toPromise';
 
+// TODO(carolynz): decide if I should initially set this.
 export const DEFAULT_ENDPOINT = '//tutorial-179815.appspot.com';
 
 @Injectable()
 export class ApiService {
-  private apiUrl: string = DEFAULT_ENDPOINT;
-
   constructor(private http: HttpClient) {}
 
-  // TODO(carolynz): add local storage.
+  // TODO(carolynz): check url format also check there is no slash at the end.
   setApiUrl(url: string) {
-    // TODO(carolynz): check url format also check there is no slash at the end.
-    this.apiUrl = url;
+    // Remove previous url.
+    localStorage.removeItem('apiUrl');
+    localStorage.setItem('apiUrl', url);
   }
 
   getApiUrl(): string {
-    return this.apiUrl;
+    return localStorage.getItem('apiUrl') || DEFAULT_ENDPOINT;
   }
 
   list(): Observable<BeverageData[]> {
-    return this.http.get(`${this.apiUrl}/list`);
+    return this.http.get(`${this.getApiUrl()}/list`);
   }
 
   addOrUpdate(beverage: BeverageData): Promise<void> {
     return this.http
-        .get(`${this.apiUrl}/put?name=${beverage.name}&value=${beverage.value}`)
+        .get(`${this.getApiUrl()}/put?name=${beverage.name}&value=${beverage.value}`)
         .toPromise()
         .then(res => res)
         .catch(err => err);
