f9f9548b5a60945931b1691d3373843803436b2d
carolynz <carolynz@google.com>
2017-10-03 14:32:09 -0400

    fix server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.use('*', function (req, res) {
-  return res.sendFile(path.join(__dirname, 'public/index.html'));
+  return res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
 // Basic error logger/handler
