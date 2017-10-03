e9dab3f8114f3fe00c29f651c820e885fcb40d56
carolynz <carolynz@google.com>
2017-10-03 14:21:05 -0400

    add node server

---
+++ server.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.use('*', function (req, res) {
+  return res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+// Basic error logger/handler
+app.use(function (err, req, res, next) {
+  res.status(500).send(err.message || 'Something broke!');
+  next(err || new Error('Something broke!'));
+});
+if (process.env.NODE_ENV === 'production') {
+  app.use(errorHandler.express);
+}
+
+if (module === require.main) {
+  // Start the server
+  const server = app.listen(process.env.port || 8080, function () {
+    const port = server.address().port;
+
+    console.log('App listening on port %s', port);
+    console.log('Press Ctrl+C to quit.');
+  });
+}
+
+module.exports = app;
